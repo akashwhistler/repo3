5137b7005553302de92951ac0711e75b7e162b57
akashwhistler <133943621+akashwhistler@users.noreply.github.com>
2023-05-19 09:42:41 +0530

    third change

--- src/gpro/c3.java
+++ src/gpro/c3.java
@@ -2,7 +2,8 @@ package gpro;
 
 public class c3 {
 	public static void main(String[] args) {
-		System.out.println("changes");
+		System.out.println(" sec changes");
+		System.out.println("conf");
 	}
 
 }
